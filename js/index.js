a501d4f21dd6d0db0cc9cfc544e97ea025e1d813
Halo <halohalo831999@gmail.com>
2022-05-09 23:34:09 +0700

    commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function() {
+
+$(".header-slider").slick({
+    infinite: true,
+    arrows: false,
+    dots: false,
+    autoplay: false,
+    lazyLoad: "ondemand",
+});
+
+})
