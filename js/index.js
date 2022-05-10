5fb2f9fdb749cbdab70011ab0c3227ed8dc17db5
Tuanna <halohalo831999@gmail.com>
2022-05-10 12:28:35 +0700

    commit

--- js/index.js
+++ js/index.js
@@ -1,12 +1,17 @@
-
 $(document).ready(function() {
 
-$(".header-slider").slick({
-    infinite: true,
-    arrows: false,
-    dots: false,
-    autoplay: false,
-    lazyLoad: "ondemand",
-});
-
+    $(".header-slider").slick({
+        infinite: true,
+        arrows: false,
+        dots: false,
+        autoplay: false,
+        lazyLoad: "ondemand",
+    });
+    $(".pack-content").slick({
+        infinite: true,
+        arrows: true,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    })
 })
