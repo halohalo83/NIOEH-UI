0ea728c505100834de0bf4cf97f7184af0a2c8e8
Tuanna <halohalo831999@gmail.com>
2022-05-11 16:31:57 +0700

    commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
-
+    $('.tab-container:first').show();
+    $('.tab-navigation li:first').addClass('active');
+    $('.tab-navigation li').click(function(event) {
+        index = $(this).index();
+        $('.tab-navigation li').removeClass('active');
+        $(this).addClass('active');
+        $('.tab-container').hide();
+        $('.tab-container').eq(index).show();
+    })
     $(".header-slider").slick({
         infinite: true,
         arrows: false,
