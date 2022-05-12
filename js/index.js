3875872addeb3dda5f21897b2194628589eee997
Tuanna <halohalo831999@gmail.com>
2022-05-12 12:00:31 +0700

    commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+    // Sự kiện click chuyển tab menu
     // $('.tab-container:first').show();
     // $('.tab-navigation li:first').addClass('active');
-    $('.tab-container.tab-water').show();
+    $('.tab-container.tab-news').show();
     $('.tab-container:first').hide();
 
     $('.tab-navigation li').click(function(event) {
@@ -10,6 +11,17 @@ $(document).ready(function() {
         $(this).addClass('active');
         $('.tab-container').hide();
         $('.tab-container').eq(index).show();
+    })
+
+    // Sự kiện chuyển tab advantage
+    $('.adv-container:first').css('display', 'flex');
+    $('.adv-navigation li:first').addClass('active');
+    $('.adv-navigation li').click(function(event) {
+        index = $(this).index();
+        $('.adv-navigation li').removeClass('active');
+        $(this).addClass('active');
+        $('.adv-container').hide();
+        $('.adv-container').eq(index).css('display', 'flex');
     })
     $(".header-slider").slick({
         infinite: true,
